c7a02ca2326bb07a59d85dc56b22307c1e860268
Stefano Formicola <stefano_17sf@libero.it>
2018-03-30 20:03:44 +0200

    Markup documentation for page 2 added. Part 1. Needed: glossary, quick receipe.

--- AnimationImages.playground/Pages/Animations.xcplaygroundpage/Contents.swift
+++ AnimationImages.playground/Pages/Animations.xcplaygroundpage/Contents.swift
@@ -7,22 +7,30 @@ let animationsViewController = AnimationsViewController()
 PlaygroundPage.current.needsIndefiniteExecution = true
 PlaygroundPage.current.liveView = animationsViewController
 
-let frames: [UIImage] = {
-    var walkingFrames: [UIImage] = []
-    
-    for i in 1...6 {
-        guard let image = UIImage(named: "Walking/bulldog\(i).png") else { continue }
-        walkingFrames.append(image)
-    }
-    
-    return walkingFrames
-}()
+private var frames: [UIImage] = []
 
 let lord = LordInterface(vc: animationsViewController)
 
 //#-end-hidden-code
 
+/*:
+ **Goal:** Animate a character.
+ 
+ Did you like the animations? Animating a character is really simple, even if it’s a bulldog! Animations like the ones you saw before are composed of lots of [frames](glossary://frame) that change quickly to give you the impression of the movement.
+ 
+ Now try to create your first animation. In the code above there’s an [array](glossary://array) of frames that you’ve to set to Lord, who’s already waiting for it in the screen.
+ 
+ * callout(How):
+ You can call Lord by writing `lord` followed by a dot, and then [pass](glossary://assignment) him what he needs for an animation. Example: `lord.animationFrames`
+ 
+ Next, why don’t you ask him to move or, maybe, just walk ?
+ 
+ * callout(Tip):
+ Lord needs to know the speed of his steps for his walking. Try to find how to give him that value.
+ 
+ */
 
+frames = [#imageLiteral(resourceName: "bulldog1.png"), #imageLiteral(resourceName: "bulldog2.png"), #imageLiteral(resourceName: "bulldog3.png"), #imageLiteral(resourceName: "bulldog4.png"), #imageLiteral(resourceName: "bulldog5.png"), #imageLiteral(resourceName: "bulldog6.png")]
 
 
 //: [Next page](@next)
